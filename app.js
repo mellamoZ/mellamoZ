33a2bbca0e0d9aba5b409c55dd5d191f002c6c15
Abdulaziz <zizoxdomi@gmail.com>
2022-05-20 17:12:43 +0300

    final

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const https = require("https");
 const BodyParser = require("body-parser");
 const { json } = require("express/lib/response");
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 app.use(BodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
@@ -36,12 +36,11 @@ app.post("/", function (req, res) {
   };
 
   const request = https.request(url, options, function (response) {
-    console.log(response);
-    // if (response.statusCode == 200) {
-    //   res.sendFile(`${__dirname}/success.html`);
-    // } else {
-    //   res.sendFile(`${__dirname}/failure.html`);
-    // }
+    if (response.statusCode == 200) {
+      res.sendFile(`${__dirname}/success.html`);
+    } else {
+      res.sendFile(`${__dirname}/failure.html`);
+    }
     response.on("data", function (data) {
       console.log(JSON.parse(data));
     });
